953682119bc8f76f529c24909d848dd2afb4256d
Yonatan Lifshitz <yonlif@gmail.com>
2019-05-20 00:41:59 +0300

    Diff fixed

--- Model/ML/FilterModel.py
+++ Model/ML/FilterModel.py
@@ -7,8 +7,35 @@ from difflib import get_close_matches
 
 
 def diff(f, s):
-    return len(get_close_matches(f, [s])) > 0
+    return editDistance(f,s,len(f),len(s))
 
+# A Naive recursive Python program to fin minimum number
+# operations to convert str1 to str2
+def editDistance(str1, str2, m, n):
+    # If first string is empty, the only option is to
+    # insert all characters of second string into first
+    if m == 0:
+        return n
+
+        # If second string is empty, the only option is to
+    # remove all characters of first string
+    if n == 0:
+        return m
+
+        # If last characters of two strings are same, nothing
+    # much to do. Ignore last characters and get count for
+    # remaining strings.
+    if str1[m - 1] == str2[n - 1]:
+        return editDistance(str1, str2, m - 1, n - 1)
+
+        # If last characters are not same, consider all three
+    # operations on last character of first string, recursively
+    # compute minimum cost for all three operations and take
+    # minimum of three values.
+    return 1 + min(editDistance(str1, str2, m, n - 1),  # Insert
+                   editDistance(str1, str2, m - 1, n),  # Remove
+                   editDistance(str1, str2, m - 1, n - 1)  # Replace
+                   )
 
 class FilterModel:
     def __init__(self, keys_file_name="good_words_list"):
