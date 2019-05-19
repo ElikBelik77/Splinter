62bfd660e85c4edf26e5bb2da889fb3dd47fa454
Yonatan Lifshitz <yonlif@gmail.com>
2019-05-19 15:13:50 +0300

    Changed file name

--- Model/ML/FilterModel.py
+++ Model/ML/FilterModel.py
@@ -1,4 +1,5 @@
-
+from .. import Message
+from .. import User
 
 class FilterModel:
     def __init__(self, keys_file_name):
@@ -12,8 +13,13 @@ class FilterModel:
         :param massages: list of Message object
         :return: Filtered list of massages
         """
-        res = self.filter_by_keys(massages) + self.filter_by_rules(massages)
-        return []
+        important_list = []
+        with open(self.keys) as file:
+            keys = file.readlines()
+            for i, msg in enumerate(massages):
+                if self.check_by_keys(msg.content, keys) or self.check_by_rules(msg):
+                    important_list.append(massages[i])
+        return important_list
 
     def personalized_filter(self, user, massages):
         """
@@ -22,11 +28,25 @@ class FilterModel:
         :param massages: list of Message object
         :return: Filtered list of massages
         """
-        res = self.filter_by_rules(massages)
-        return []
+        important_list = []
+        for i, msg in enumerate(massages):
+            if self.check_by_keys(msg.content, user.preferences) or self.check_by_rules(msg):
+                important_list.append(massages[i])
+        return important_list
 
-    def filter_by_keys(self, massages):
+    def check_by_keys(self, massage, keys):
+        """
+        Check a single massage by keys
+        :param massage:
+        :param keys:
+        :return:
+        """
         pass
 
-    def filter_by_rules(self, massages):
+    def check_by_rules(self, massage):
+        """
+        Check a single massage by rules
+        :param massage:
+        :return:
+        """
         pass
