1bc690167d39d6d50e8da8ee87a79b1a7922edf0
ElikBelik77 <Eliran2001@gmail.com>
2019-05-19 14:41:58 +0300

    finished user dialog logic.

--- Controller/Controller.py
+++ Controller/Controller.py
@@ -1,6 +1,7 @@
 import time
 import threading
 from Controller import UserDialogLogic
+from Model import Message
 
 
 class Controller:
@@ -11,6 +12,7 @@ class Controller:
         self.last_digest = time.time()
         self.digest_timer = threading.Timer(self.digest_interval, self.interval_function)
         self.user_interaction_parser = UserDialogLogic.UserDialogLogic()
+        self.whats_app_model = None
 
     def stop(self):
         self.stop = True
@@ -20,3 +22,10 @@ class Controller:
         for activity in self.activities:
             activity()
         tick_processing_time = start_time - time.time()
+
+    def on_message_received(self, messages: Message):
+        for message in messages:
+            if message.sender == message.group:
+                self.user_interaction_parser.handle(message, self.whats_app_model.sender)
+            else:
+                self.whats_app_model.do(message)
--- Controller/UserDialogLogic.py
+++ Controller/UserDialogLogic.py
@@ -1,6 +1,16 @@
+from Model import Message
+
+
 class UserDialogLogic:
     def __init__(self):
+        self.user_dialogs = {}
         pass
 
-    def extract(self):
-        pass
+    def handle(self, message: Message, whats_app_sender):
+        # TODO: if there is time, implement a better dialog system ?
+        if message.sender not in self.user_dialogs:
+            whats_app_sender.send("Noted ! Hope you have a good time with bot")
+            self.user_dialogs[message.sender] = 1
+        else:
+            whats_app_sender.send("Noted ! Hope you have a good time with bot")
+        return message.content.split(',')
