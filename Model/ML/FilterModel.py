1daab3eaf1c530cf042103bd9585bb7e1bd2b1c7
Yonatan Lifshitz <yonlif@gmail.com>
2019-05-19 15:26:28 +0300

    Fixed typos

--- Model/ML/FilterModel.py
+++ Model/ML/FilterModel.py
@@ -1,52 +1,61 @@
 from .. import Message
 from .. import User
+import re
 
 class FilterModel:
     def __init__(self, keys_file_name):
         # File name of default key words
         self.keys = keys_file_name
 
-    def filter(self, massages):
+    def filter(self, messages):
         """
-        Filter a list of massages
+        Filter a list of messages
 
-        :param massages: list of Message object
-        :return: Filtered list of massages
+        :param messages: list of Message object
+        :return: Filtered list of messages
         """
         important_list = []
         with open(self.keys) as file:
             keys = file.readlines()
-            for i, msg in enumerate(massages):
+            for i, msg in enumerate(messages):
                 if self.check_by_keys(msg.content, keys) or self.check_by_rules(msg):
-                    important_list.append(massages[i])
+                    important_list.append(messages[i])
         return important_list
 
-    def personalized_filter(self, user, massages):
+    def personalized_filter(self, user, messages):
         """
-        Filter a list of massages according to specific user
+        Filter a list of messages according to specific user
 
-        :param massages: list of Message object
-        :return: Filtered list of massages
+        :param user: User object
+        :param messages: list of Message object
+        :return: Filtered list of messages
         """
         important_list = []
-        for i, msg in enumerate(massages):
+        for i, msg in enumerate(messages):
             if self.check_by_keys(msg.content, user.preferences) or self.check_by_rules(msg):
-                important_list.append(massages[i])
+                important_list.append(messages[i])
         return important_list
 
-    def check_by_keys(self, massage, keys):
+    def check_by_keys(self, message, keys):
         """
-        Check a single massage by keys
-        :param massage:
+        Check a single message by keys
+        :param message:
         :param keys:
         :return:
         """
         pass
 
-    def check_by_rules(self, massage):
+    def check_by_rules(self, message):
         """
-        Check a single massage by rules
-        :param massage:
+        Check a single message by rules
+        :param message:
         :return:
         """
-        pass
+        if len(message) < 2:
+            return False
+        if ':' in message:
+            return True
+        words = message.split(" ")
+        return False
+
+
